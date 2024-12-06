265dfa7442a2a105012725221050982a294a38f5
Lovie-Tonimba <jechonialovie@gmail.com>
2024-12-06 19:51:00 +0700

    experiment 3

---
+++ ProvitRecursive244107060101.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ProvitRecursive244107060101 {
+    static double calculateProfit(double balance, int period){
+        if(period == 0){
+            return balance;
+        }else{
+            return 1.11*calculateProfit(balance, period-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input Balance: ");
+        double initialBalance = input.nextDouble();
+        System.out.print("Input Invesment Period: ");
+        int investPeriod = input.nextInt();
+        
+        System.out.println("Balance after " + investPeriod + " year = " + calculateProfit(initialBalance, investPeriod));
+    }
+}
