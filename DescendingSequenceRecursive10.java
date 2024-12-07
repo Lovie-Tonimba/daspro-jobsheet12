d3670158261081c60eadf2081ac364d964a75c3b
Lovie-Tonimba <jechonialovie@gmail.com>
2024-12-07 11:02:02 +0700

    assignment no 1

--- DescendingSequenceRecursive10.java
+++ DescendingSequenceRecursive10.java
@@ -1,22 +1,33 @@
-
 import java.util.Scanner;
-
 public class DescendingSequenceRecursive10 {
     static int descendingSequenceIterative(int n){
-        int sequence = 0;
-        for (int i = n; i > 0; i--) {
-            sequence = 0;
-            sequence += i;
-            System.out.println(sequence);
+        for (int i = n; i >= 0; i--) {
+            System.out.println(i);
         }
-        return sequence;
+        return 0;
+    }
+    static int descendingSequenceRecursive(int n){
+        if(n == -1){
+            return 0;
+        }else{
+            System.out.println(n);
+            return descendingSequenceRecursive(n-1);
+        }
+        //another code
+        // if(n >= 0){
+        //     System.out.println(n);
+        //     return descendingSequenceRecursive(n-1);
+        // }else{
+        //     return 0;
+        // }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Input numbers: ");
         int numbers = sc.nextInt();
-
-        System.out.println(descendingSequenceIterative(numbers));
+        System.out.println("Iterative function: ");
+        descendingSequenceIterative(numbers);
+        System.out.println("Recursive function: ");
+        descendingSequenceRecursive(numbers);
     }
 }
