efb463a6e5accd1db60ba90f0ca3fb57eb91a9a2
Lovie-Tonimba <jechonialovie@gmail.com>
2024-12-07 16:33:02 +0700

    modif experiment 2

--- PowerRecursive244107060101.java
+++ PowerRecursive244107060101.java
@@ -7,6 +7,15 @@ public class PowerRecursive244107060101 {
             return base * calculatePower(base, pow-1);
         }
     }
+    static String calculatePowerString(int base, int pow){
+        if(pow == 0){
+            return "1";
+        }else if(pow == 1){
+            return String.valueOf(base);
+        }else{
+            return base + "x" + calculatePowerString(base, pow - 1);
+        }
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Input Base Number: ");
@@ -14,6 +23,8 @@ public class PowerRecursive244107060101 {
         System.out.print("Input Power Number: ");
         int power = input.nextInt();
         
-        System.out.println("Result of " + base + " power " + power + " = " + calculatePower(base, power));
+        // System.out.println("Result of " + base + " power " + power + " = " + calculatePower(base, power));
+        String modif = calculatePowerString(base, power);
+        System.out.println(modif + "x1 = " + calculatePower(base, power));
     }
 }
